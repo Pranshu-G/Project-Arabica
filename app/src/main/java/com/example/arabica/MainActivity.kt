c97f1dc2810678eacbd93f578586c6d297372699
Pranshu-G <pranshu.grover18@gmail.com>
2023-12-26 23:36:16 +0530

    Project Arabica screen

--- app/src/main/java/com/example/arabica/MainActivity.kt
+++ app/src/main/java/com/example/arabica/MainActivity.kt
@@ -16,31 +16,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ArabicaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("Project Arabica")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArabicaTheme {
-        Greeting("Android")
-    }
-}
